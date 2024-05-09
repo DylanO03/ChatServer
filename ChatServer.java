ff78a1372495855182c5b6bee69a8132b70b6a32
DylanO <dylantoquendo@icloud.com>
2024-05-08 22:07:59 -0700

    Add files via upload

--- ChatServer.java
+++ ChatServer.java
@@ -1,15 +1,10 @@
 import java.io.IOException;
 import java.net.URI;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 
 class ChatHandler implements URLHandler {
   String chatHistory = "";
 
   public String handleRequest(URI url) {
-
-    // expect /chat?user=<name>&message=<string>
 
     // expect /add-message?s=<string>&user=<string>
     if (url.getPath().equals("/add-message")) {
@@ -25,45 +20,8 @@ class ChatHandler implements URLHandler {
         return "Invalid parameters: " + String.join("&", params);
       }
     }
-    // expect /retrieve-history?file=<name> 
-    /* 
-    else if (url.getPath().equals("/retrieve-history")) {
-      String[] params = url.getQuery().split("&");
-      String[] shouldBeFile = params[0].split("=");
-      if (shouldBeFile[0].equals("file")) {
-        String fileName = shouldBeFile[1];
-        // String fileName = shouldBeFileName[0]; // bug4: should be shouldBeFile[1]
-        ChatHistoryReader reader = new ChatHistoryReader();
-        try {
-          String[] contents = reader.readFileAsArray("chathistory/" + fileName);
-          for (String line : contents) {
-            this.chatHistory += line + "\n\n";
-          }
-        } catch (IOException e) {
-          System.err.println("Error reading file: " + e.getMessage());
-        }
-      }
-      return this.chatHistory;
-    }
-    // expect /save?name=<name>
-    else if (url.getPath().equals("/save")) {
-      String[] params = url.getQuery().split("&");
-      String[] shouldBeFileName = params[0].split("=");
-      if (shouldBeFileName[0].equals("name")) {
-        File directory = new File("chathistory");
-        File file = new File(directory, shouldBeFileName[1]);
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-          writer.write(this.chatHistory);
-          return "Data written to " + shouldBeFileName[1] + "in 'chat-history' folder.";
-        } catch (IOException e) {
-          e.printStackTrace();
-          return "Error: Something wrong happen during file save, check StackTrace";
-        }
-      }
-    } 
-    return "404 Not Found";
-  } */
+    return this.chatHistory;
+  } 
 }
 
 class ChatServer {
